c581abfe0d68ec70bcf953fe71327fe009c93927
frenk1j <frenki.janaqi@icloud.com>
2025-10-19 15:16:11 +0000

    western exchange

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 """
 Module 11-the_western_exchange
-Transposes a matrix without importing any module.
+Transposes a 2D matrix without loops, conditionals, or imports.
 """
 
 
 def np_transpose(matrix):
-    """Returns the transpose of a matrix"""
-    try:
-        return [
-            [matrix[j][i] for j in range(len(matrix))]
-            for i in range(len(matrix[0]))
-        ]
-    except (TypeError, IndexError):
-        return matrix
+    """Returns the transpose of a 2D matrix"""
+    return list(map(list, zip(*matrix)))
