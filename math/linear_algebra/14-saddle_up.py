1f601937b3f034df39522ddca82fa4c790eafa47
frenk1j <frenki.janaqi@icloud.com>
2025-10-19 15:30:03 +0000

    saddle up

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module 14-saddle_up
+Performs matrix multiplication of two numpy.ndarrays without loops.
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Return a new numpy.ndarray which is the product of mat1 and mat2"""
+    return np.matmul(mat1, mat2)
