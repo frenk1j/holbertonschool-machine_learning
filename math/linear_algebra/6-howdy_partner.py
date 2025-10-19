a7a992b9beae53969dcb0919863985273da682ea
frenk1j <frenki.janaqi@icloud.com>
2025-10-19 10:52:04 +0000

    howdy partner

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a function that concatenates two arrays."""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenates two arrays and returns a new array."""
+    catted_array = arr1 + arr2
+    return catted_array
