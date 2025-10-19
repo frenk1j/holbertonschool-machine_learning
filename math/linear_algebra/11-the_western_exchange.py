e172fbf196ad9d67c52450c2048ef767608a8a67
frenk1j <frenki.janaqi@icloud.com>
2025-10-19 15:13:49 +0000

    western exchange

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
-import numpy as np
+"""
+Module 11-the_western_exchange
+Transposes a matrix without importing any module.
+"""
 
 
 def np_transpose(matrix):
-    """Returns the transpose of a numpy.ndarray"""
-    return matrix.T
+    """Returns the transpose of a matrix"""
+    try:
+        return [
+            [matrix[j][i] for j in range(len(matrix))]
+            for i in range(len(matrix[0]))
+        ]
+    except (TypeError, IndexError):
+        return matrix
