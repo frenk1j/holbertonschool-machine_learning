500b2d0da2ef5cc97df55dfd13272fbb23fcdb68
frenk1j <frenki.janaqi@icloud.com>
2025-10-19 15:07:51 +0000

    use my scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module 10-ill_use_my_scale
+Calculates the shape of a numpy.ndarray.
+"""
+
+
+def np_shape(matrix):
+    """Returns the shape of a numpy.ndarray"""
+    return matrix.shape
