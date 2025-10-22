29d602f113e8549d421dcc73e7daf7daf79da345
frenk1j <frenki.janaqi@icloud.com>
2025-10-22 07:52:56 +0000

    17-integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,24 +1,28 @@
+#!/usr/bin/env python3
+"""Calculates the integral of a polynomial"""
+
+
 def poly_integral(poly, C=0):
-    # Validate inputs
-    if not isinstance(poly, list) or len(poly) == 0:
+    """Calculates the integral of a polynomial"""
+    if (not isinstance(poly, list) or
+            len(poly) == 0 or
+            not all(isinstance(x, (int, float)) for x in poly) or
+            not isinstance(C, (int, float))):
         return None
-    if not isinstance(C, int):
-        return None
-    for c in poly:
-        if not isinstance(c, (int, float)):
-            return None
 
-    # Build integral coefficients: start with constant C
-    res = [C]
-    for power, coef in enumerate(poly):
-        new_coef = coef / (power + 1)
-        # Cast to int if it is an integer value
-        if isinstance(new_coef, float) and new_coef.is_integer():
-            new_coef = int(new_coef)
-        res.append(new_coef)
+    # fillojmë me konstanten C
+    integral = [C]
 
-    # Trim trailing zeros to keep list as small as possible
-    while len(res) > 1 and res[-1] == 0:
-        res.pop()
+    # për çdo element të poly, ndajmë me (index + 1)
+    for i in range(len(poly)):
+        value = poly[i] / (i + 1)
+        # ktheje në int nëse është numër i plotë
+        if value.is_integer():
+            value = int(value)
+        integral.append(value)
 
-    return res
+    # hiq zerot e panevojshme në fund
+    while len(integral) > 1 and integral[-1] == 0:
+        integral.pop()
+
+    return integral
