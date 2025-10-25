b448da8504bb44787b18ef74d7f884addf67ab37
frenk1j <frenki.janaqi@icloud.com>
2025-10-25 08:25:00 +0000

    normal

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -31,32 +31,33 @@ class Normal:
             self.stddev = float(variance ** 0.5)
 
     def z_score(self, x):
-        """Calculates the z-score of a given x-value
-        Args:
-            x (float): x-value
-        Returns:
-            float: z-score of x
-        """
+        """Calculates the z-score of a given x-value"""
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
-        """Calculates the x-value of a given z-score
-        Args:
-            z (float): z-score
-        Returns:
-            float: x-value corresponding to z
-        """
+        """Calculates the x-value of a given z-score"""
         return (z * self.stddev) + self.mean
 
     def pdf(self, x):
-        """Calculates the value of the PDF for a given x-value
-        Args:
-            x (float): x-value
-        Returns:
-            float: PDF value for x
-        """
+        """Calculates the value of the PDF for a given x-value"""
         pi = 3.1415926536
         e = 2.7182818285
         coeff = 1 / (self.stddev * ((2 * pi) ** 0.5))
         exponent = e ** (-0.5 * ((x - self.mean) / self.stddev) ** 2)
         return coeff * exponent
+
+    def cdf(self, x):
+        """Calculates the value of the CDF for a given x-value
+        Args:
+            x (float): x-value
+        Returns:
+            float: CDF value for x
+        """
+        # Formula using the error function approximation
+        pi = 3.1415926536
+        z = (x - self.mean) / (self.stddev * (2 ** 0.5))
+        # Approximation for error function erf(z)
+        erf = (2 / (pi ** 0.5)) * (
+            z - (z ** 3) / 3 + (z ** 5) / 10 - (z ** 7) / 42 + (z ** 9) / 216
+        )
+        return 0.5 * (1 + erf)
