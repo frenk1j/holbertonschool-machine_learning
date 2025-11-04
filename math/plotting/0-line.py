57aa767043840e08b57a1df747ed7dc28c1c2026
frenk1j <frenki.janaqi@icloud.com>
2025-11-04 18:36:56 +0000

    line

---
+++ math/plotting/0-line.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line():
+    """Plots y as a line graph"""
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    x = np.arange(0, 11)
+    plt.plot(x, y, 'r-')  # r = red, - = solid line
+    plt.xlim(0, 10)
+    plt.show()
