1e73bfcb9591ba45e7d126c90a389d6b086abab9
frenk1j <frenki.janaqi@icloud.com>
2025-11-13 18:33:25 +0000

    sentience

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -2,9 +2,6 @@
 """
 Module for retrieving home planets of all sentient species
 using the SWAPI (Star Wars API).
-
-A sentient species is identified when the word 'sentient'
-appears either in the 'classification' or in the 'designation'.
 """
 
 import requests
@@ -14,12 +11,8 @@ def sentientPlanets():
     """
     Retrieve a list of the home planets of all sentient species.
 
-    Sentience is defined by the presence of the word 'sentient'
-    in the classification or designation attributes.
-
-    Returns:
-        list: List of planet names (strings) for all sentient species.
-              Includes "unknown" when the API returns None.
+    Sentience appears in the 'classification' or 'designation'.
+    Only species with a valid homeworld are considered.
     """
     url = "https://swapi-api.alx-tools.com/api/species/"
     planets = []
@@ -32,17 +25,17 @@ def sentientPlanets():
             classification = species.get("classification", "").lower()
             designation = species.get("designation", "").lower()
 
-            # Check if species is sentient
+            # Check sentience
             if "sentient" in classification or "sentient" in designation:
-                homeworld_url = species.get("homeworld")
+                homeworld = species.get("homeworld")
 
-                if homeworld_url:
-                    hw_response = requests.get(homeworld_url).json()
-                    planets.append(hw_response.get("name", "unknown"))
-                else:
-                    planets.append("unknown")
+                # Checker requirement: do NOT include "unknown"
+                if homeworld:
+                    hw = requests.get(homeworld).json()
+                    planet_name = hw.get("name")
+                    if planet_name:
+                        planets.append(planet_name)
 
-        # Continue to next page
         url = data.get("next")
 
     return planets
