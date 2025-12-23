1aa21a66d9630ac7d585f05c6a6901ed1985249b
frenk1j <frenki.janaqi@icloud.com>
2025-12-23 15:31:11 +0000

    BIC

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -10,6 +10,7 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None
     if not isinstance(kmin, int) or kmin <= 0:
         return None, None, None, None
+
     n, d = X.shape
     if kmax is None:
         kmax = n
@@ -22,30 +23,33 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     if not isinstance(verbose, bool):
         return None, None, None, None
 
-    k_values = list(range(kmin, kmax + 1))
+    k_values = np.arange(kmin, kmax + 1)
     results_history = []
     lh_history = []
     bic_history = []
 
+    # Only one loop allowed
     for k in k_values:
         pi, m, S, g, lh = expectation_maximization(
             X, k, iterations=iterations, tol=tol, verbose=verbose
         )
-
-        if pi is None or m is None or S is None or g is None or lh is None:
+        if None in (pi, m, S, g, lh):
             return None, None, None, None
 
         # Number of parameters for GMM:
         # (k-1) priors + k*d means + k*d*(d+1)/2 covariances
         p = (k - 1) + k * d + k * d * (d + 1) / 2
-        bic_value = p * np.log(n) - 2 * lh
+
+        bic_value = (
+            p * np.log(n) - 2 * lh
+        )  # split line to pass pycodestyle E501
 
         results_history.append((pi, m, S))
         lh_history.append(lh)
         bic_history.append(bic_value)
 
-    min_bic_index = np.argmin(bic_history)
-    best_k = k_values[min_bic_index]
+    min_bic_index = int(np.argmin(bic_history))
+    best_k = int(k_values[min_bic_index])
     best_result = results_history[min_bic_index]
 
     return best_k, best_result, np.array(lh_history), np.array(bic_history)
