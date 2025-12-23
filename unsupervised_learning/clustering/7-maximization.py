ba352c66c350cc7aed07e368e8fe608143674fe8
frenk1j <frenki.janaqi@icloud.com>
2025-12-23 13:15:56 +0000

    maximization

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -4,7 +4,7 @@ kmeans = __import__('1-kmeans').kmeans
 
 
 def maximization(X, g):
-    """Maximization step for EM algorithm on GMM"""
+    """Maximization step for EM algorithm on GMM without multiple loops"""
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
     if not isinstance(g, np.ndarray) or len(g.shape) != 2:
@@ -15,15 +15,21 @@ def maximization(X, g):
     n, d = X.shape
     k = g.shape[0]
 
+    # Validate probabilities sum to 1 for each point
     if not np.isclose(np.sum(g, axis=0), np.ones(n)).all():
         return None, None, None
 
+    # Mixing coefficients
     pi = np.sum(g, axis=1) / n
+
+    # Means
     m = np.dot(g, X) / np.sum(g, axis=1)[:, np.newaxis]
 
-    S = np.zeros((k, d, d))
-    for i in range(k):
-        X_centered = X - m[i]
-        S[i] = (g[i][:, np.newaxis] * X_centered).T @ X_centered / np.sum(g[i])
+    # Covariances (vectorized)
+    X_expanded = X[np.newaxis, :, :] - m[:, np.newaxis, :]  # shape (k, n, d)
+    g_expanded = g[:, :, np.newaxis]                        # shape (k, n, 1)
+    S = np.einsum(
+        'kni,kni->kij', g_expanded * X_expanded, X_expanded
+    ) / np.sum(g, axis=1)[:, np.newaxis, np.newaxis]
 
     return pi, m, S
