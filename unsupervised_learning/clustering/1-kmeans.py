9d1a463381bcd6fcf354c0e1b33af9d396d7c38b
frenk1j <frenki.janaqi@icloud.com>
2025-12-23 12:44:23 +0000

    initialize

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -8,16 +8,7 @@ import numpy as np
 
 
 def initialize(X, k):
-    """
-    Initializes centroids using a multivariate uniform distribution.
-
-    Parameters:
-    X (numpy.ndarray): Dataset of shape (n, d)
-    k (int): Number of clusters
-
-    Returns:
-    numpy.ndarray: Initialized centroids of shape (k, d)
-    """
+    """Initializes centroids using a multivariate uniform distribution."""
     min_vals = np.min(X, axis=0)
     max_vals = np.max(X, axis=0)
     return np.random.uniform(min_vals, max_vals, size=(k, X.shape[1]))
@@ -27,16 +18,10 @@ def kmeans(X, k, iterations=1000):
     """
     Performs K-means clustering.
 
-    Parameters:
-    X (numpy.ndarray): Dataset of shape (n, d)
-    k (int): Number of clusters
-    iterations (int): Maximum number of iterations
-
     Returns:
-    tuple:
-        - C (numpy.ndarray): Centroids, shape (k, d)
-        - clss (numpy.ndarray): Cluster index for each data point, shape (n,)
-        Returns (None, None) on failure
+        C: numpy.ndarray of shape (k, d) of centroids
+        clss: numpy.ndarray of shape (n,) of cluster indices
+        or (None, None) on failure
     """
     if (not isinstance(X, np.ndarray) or X.ndim != 2
             or not isinstance(k, int) or k <= 0
@@ -49,7 +34,7 @@ def kmeans(X, k, iterations=1000):
 
     for _ in range(iterations):
         old_C = C.copy()
-        # Compute distances and assign clusters
+        # Assign clusters
         dist = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
         clss = np.argmin(dist, axis=1)
 
@@ -62,7 +47,7 @@ def kmeans(X, k, iterations=1000):
             else:
                 C[i] = points.mean(axis=0)
 
-        # Early stopping if centroids do not change
+        # Early stopping
         if np.allclose(C, old_C):
             break
 
