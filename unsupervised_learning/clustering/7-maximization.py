f5ff48ce7d650ac564dd6b5093122dc86a19db40
frenk1j <frenki.janaqi@icloud.com>
2025-12-24 13:04:46 +0000

    maximization

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -1,34 +1,48 @@
 #!/usr/bin/env python3
+"""Maximization step in EM algorithm for GMM"""
 import numpy as np
-kmeans = __import__('1-kmeans').kmeans
 
 
 def maximization(X, g):
-    """Maximization step for EM algorithm on GMM without multiple loops"""
+    """
+    Calculates the maximization step in the EM algorithm for a GMM
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the data set
+        g: numpy.ndarray of shape (k, n) containing posterior probabilities
+
+    Returns:
+        pi: numpy.ndarray of shape (k,) containing updated priors
+        m: numpy.ndarray of shape (k, d) containing updated means
+        S: numpy.ndarray of shape (k, d, d) containing updated covariances
+        or None, None, None on failure
+    """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
     if not isinstance(g, np.ndarray) or len(g.shape) != 2:
         return None, None, None
-    if X.shape[0] != g.shape[1]:
-        return None, None, None
 
     n, d = X.shape
-    k = g.shape[0]
+    k, n_g = g.shape
 
-    # Validate probabilities sum to 1 for each point
-    if not np.isclose(np.sum(g, axis=0), np.ones(n)).all():
+    if n != n_g:
         return None, None, None
 
-    # Mixing coefficients
-    pi = np.sum(g, axis=1) / n
+    if not np.allclose(np.sum(g, axis=0), 1):
+        return None, None, None
 
-    # Means
-    m = np.dot(g, X) / np.sum(g, axis=1)[:, np.newaxis]
+    try:
+        N_k = np.sum(g, axis=1)
+        pi = N_k / n
+        m = (g @ X) / N_k[:, np.newaxis]
 
-    # Covariances (vectorized, only one loop)
-    S = np.zeros((k, d, d))
-    for i in range(k):  # maximum one loop allowed
-        X_centered = X - m[i]
-        S[i] = (g[i][:, np.newaxis] * X_centered).T @ X_centered / np.sum(g[i])
+        S = np.zeros((k, d, d))
 
-    return pi, m, S
+        for i in range(k):
+            X_centered = X - m[i]
+            weighted = g[i, :, np.newaxis] * X_centered
+            S[i] = (weighted.T @ X_centered) / N_k[i]
+
+        return pi, m, S
+    except Exception:
+        return None, None, None
