fec50329d70cc82989bc95980465a099d1cb8881
frenk1j <frenki.janaqi@icloud.com>
2026-02-17 12:04:53 +0100

    Your message

---
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Performs a convolution on grayscale images with padding and stride
+"""
+
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """
+    Performs a convolution on grayscale images
+
+    Parameters:
+    images (numpy.ndarray): shape (m, h, w)
+    kernel (numpy.ndarray): shape (kh, kw)
+    padding (tuple, str): 'same', 'valid', or (ph, pw)
+    stride (tuple): (sh, sw)
+
+    Returns:
+    numpy.ndarray: convolved images
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2 + 1 * (
+            ((h - 1) * sh + kh - h) % 2 != 0
+        )
+        pw = ((w - 1) * sw + kw - w) // 2 + 1 * (
+            ((w - 1) * sw + kw - w) % 2 != 0
+        )
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    padded_images = np.pad(
+        images,
+        pad_width=((0, 0), (ph, ph), (pw, pw)),
+        mode='constant'
+    )
+
+    output_h = (h + 2 * ph - kh) // sh + 1
+    output_w = (w + 2 * pw - kw) // sw + 1
+
+    output = np.zeros((m, output_h, output_w))
+
+    for i in range(output_h):
+        for j in range(output_w):
+            h_start = i * sh
+            w_start = j * sw
+            region = padded_images[
+                :, h_start:h_start + kh, w_start:w_start + kw
+            ]
+            output[:, i, j] = np.sum(region * kernel, axis=(1, 2))
+
+    return output
