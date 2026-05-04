647f3d59a30d26bd521552ba40bd4f0f999434b0
frenk1j <frenki.janaqi@icloud.com>
2026-05-04 17:18:44 +0200

    word

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -4,37 +4,24 @@ import tensorflow as tf
 
 
 def gensim_to_keras(model):
-    """Convert a gensim word2vec model to a trainable Keras Embedding layer.
-
-    Args:
-        model: a trained gensim word2vec model.
-
-    Returns:
-        a trainable keras Embedding layer.
-    """
-    features = sorted(
-        model.wv.index_to_key,
-        key=lambda word: (
-            model.wv.get_vecattr(word, "count"),
-            model.wv.key_to_index[word]
-        ),
-        reverse=True
-    )
-    order = [model.wv.key_to_index[word] for word in features]
-
-    model.wv.vectors = model.wv.vectors[order]
-    model.wv.index_to_key = features
-    model.wv.key_to_index = {word: i for i, word in enumerate(features)}
-    for attr, values in model.wv.expandos.items():
-        model.wv.expandos[attr] = values[order]
-
+    """Convert a gensim word2vec model to a trainable Keras Embedding layer."""
+    
+    # Merr direkt weights
     weights = model.wv.vectors
+    
     vocab_size, vector_size = weights.shape
+    
+    # Krijo embedding layer
     embedding_layer = tf.keras.layers.Embedding(
         input_dim=vocab_size,
         output_dim=vector_size,
         trainable=True
     )
+    
+    # Build layer që të pranojë weights
     embedding_layer.build((None,))
+    
+    # Vendos weights
     embedding_layer.set_weights([weights])
+    
     return embedding_layer
