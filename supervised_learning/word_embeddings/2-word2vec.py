f2ef889b516bf4e7d8ffa70b1f62f88b069c7911
frenk1j <frenki.janaqi@icloud.com>
2026-05-04 17:45:26 +0200

    word

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
-"""Train a gensim Word2Vec model."""
-import gensim
+"""Train a gensim Word2Vec model"""
+from gensim.models import Word2Vec
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
-                   cbow=True, epochs=5, seed=0, workers=1):
-    """Create and return a trained Word2Vec model."""
-    
+                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
+    """Creates, builds and trains a gensim word2vec model"""
     sg = 0 if cbow else 1
-    
-    model = gensim.models.Word2Vec(
+    model = Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
-        window=window,
         min_count=min_count,
+        window=window,
+        negative=negative,
         sg=sg,
         epochs=epochs,
         seed=seed,
         workers=workers
     )
-    
     return model
--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python3
-"""Convert a gensim word2vec model to a Keras Embedding layer."""
-import tensorflow as tf
+"""Convert a gensim Word2Vec model to a Keras Embedding layer"""
+import numpy as np
+from keras.layers import Embedding
 
 
 def gensim_to_keras(model):
-    """Convert a gensim word2vec model to a trainable Keras Embedding layer."""
-    
-    # Merr direkt weights
+    """Converts a gensim word2vec model to a trainable Keras Embedding layer"""
     weights = model.wv.vectors
-    
     vocab_size, vector_size = weights.shape
-    
-    # Krijo embedding layer
-    embedding_layer = tf.keras.layers.Embedding(
+    embedding = Embedding(
         input_dim=vocab_size,
         output_dim=vector_size,
+        weights=[weights],
         trainable=True
     )
-    
-    # Build layer që të pranojë weights
-    embedding_layer.build((None,))
-    
-    # Vendos weights
-    embedding_layer.set_weights([weights])
-    
-    return embedding_layer
+    return embedding
